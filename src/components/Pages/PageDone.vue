82b7ba3aa185a4444e6de10874f7f936991a0602
Maxime De Mey <maxime.demey@duratech.be>
2024-06-04 14:13:43 +0200

    Add "Update another device" button to PageDone.vue

--- src/components/Pages/PageDone.vue
+++ src/components/Pages/PageDone.vue
@@ -2,6 +2,8 @@
     <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 text-center">
         <div class="container py-10 ">
             <h3 class="text-xl font-semibold">Update Successful!</h3>
+            <button class="bg-durablue text-white py-2 px-24 rounded-md my-10 text-xl transition delay-100 hover:bg-duraaccent"
+        @click="location.reload();">Update another device</button>
         </div>
     </div>
 </template>
